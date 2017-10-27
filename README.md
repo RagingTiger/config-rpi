12b7d655868105d2a4f8bfd44bbaddab26d7fd45
01101010 <jander43@vols.utk.edu>
2017-10-27 13:45:19 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## About
+This is a simple bash script created from [Gordon Lesti's](https://gordonlesti.com/change-default-users-on-raspberry-pi/)
+tutorial on setting up a new user for the **raspberry pi**, and **deleting**
+the **pi** user.
---
+++ create-user.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# Reference: https://gordonlesti.com/change-default-users-on-raspberry-pi/
+
+setup_user(){
+  # first add new user
+  sudo adduser $1
+
+  # now add to sudo group
+  sudo adduser $1 sudo
+}
+
+setup_sshkey(){
+  # path to .ssh dir
+  local ssh_dir="/home/${1}/.ssh"
+
+  # setup .ssh dir in $HOME
+  sudo mkdir $ssh_dir
+
+  # change ownership
+  sudo chown ${1}:${1} $ssh_dir
+
+  # now instruct user on how to copy keys
+  echo "Execute on your local machine (assumes rpi is on local network):"
+  echo "  $ scp ~/.ssh/id_rsa.pub ${1}@$(hostname):/home/${1}/.ssh/authorized_keys"
+  echo ""
+
+
+  # now confirm keys are copied, print next steps, and logout
+  echo "After keys are copied, logout of pi user and execute the following: "
+  echo "  $ ssh ${1}@$(hostname)"
+  echo ""
+  echo "Followed by:"
+  echo "  $ sudo deluser pi"
+  exit
+}
+
+main(){
+  # get new user
+  echo -n "Enter name of new raspberrypi user: "
+  local newuser
+  read newuser
+
+  # check user
+  if [[ $newuser ]]; then
+      # setup new user
+      setup_user "$newuser"
+
+      # setup new ssh key
+      setup_sshkey "$newuser"
+
+  else
+      echo "User not supplied (Your user was \"${newuser}\". Exiting ..."
+      exit
+  fi
+}
+
+main
